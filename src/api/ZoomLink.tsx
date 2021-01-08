f0787b8cce66439fdeddb42e26aff476e319d51d
Johnny Bui <typngeek@aim.com>
2021-01-08 18:34:59 -0500

    implement zoom link parsing

--- src/api/ZoomLink.tsx
+++ src/api/ZoomLink.tsx
@@ -1,16 +1,24 @@
-var Url = require('url-parse');
 
-export interface IZoomLinkParams {
-  domain: string,
-  meetingId: string,
-}
-
+// given an input into the zoom link box, return a room name or null if invalid
 export const parseZoomLink = (zoomLink: string) => {
-  const url = new URL(zoomLink);
-  const domain = url.hostname.split(".")[0];
-  const pathList = url.pathname.split("/");
-  const meetingId = pathList[pathList.length - 1];
-  const result: IZoomLinkParams = { domain, meetingId };
-  return result;
+  // check if zoom link matches regex
+  const zoomLinkRegex = /^http(s)?:\/\/.*.?zoom.us\/j\/\d*(#.*)?$/;
+  const zoomLinkFound = zoomLink.match(zoomLinkRegex);
+
+  // check if 9-11 digits with arbitrary whitespace in between
+  const meetingIdRegex = /^( *[0-9] *){9,11}$/;
+  const meetingIdFound = zoomLink.match(meetingIdRegex);
+  
+  if (zoomLinkFound) {
+    const url = new URL(zoomLink);
+    const pathList = url.pathname.split("/");
+    const meetingId = pathList[pathList.length - 1];
+    return meetingId.toString();
+  } else if (meetingIdFound) {
+    return zoomLink.replaceAll(' ', '');
+  } else {
+    return null;
+  }
+
 }
 
